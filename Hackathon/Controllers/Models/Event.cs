49cef124591224ec81a8befa18f2c7066ef4a23e
adwaith radhakrishnan <adwaith.radhakrishnan@hrblock.ca>
2024-06-19 16:04:48 +0530

    backend initial commit

---
+++ Hackathon/Controllers/Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace Cangonito.Controllers.Models
+{
+    public class Event
+    {
+        public string EventName { get; set; }
+        public string EventStatus { get; set; }
+        public string Error { get; set; }
+    }
+}
